d23826f66c7ee5759e97fec4ec66664115dd09c7
Terence Mangram <tmangram@gmail.com>
2018-01-25 19:07:00 +0900

    Fix error from referencing wrong dirs and files

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,42 @@
-let path = require("path");
-let nodeExternals = require("webpack-node-externals");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+let path = require('path');
+let nodeExternals = require('webpack-node-externals');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 const moduleObj = {
 	loaders: [
 		{
 			test: /\.js$/,
 			exclude: /node_modules/,
-			loaders: ["babel-loader"],
+			loaders: ['babel-loader'],
 		}
 	]
 };
 
 const client = {
 	entry: {
-		"client": "./src/client/index.js"
+		'client': './src/client/index.js'
 	},
-	target: "web",
+	target: 'web',
 	output: {
-		filename: "[name].js",
-		path: path.resolve(__dirname, "public/dist")
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'dist/public')
 	},
 	module: moduleObj,
 	plugins: [
 		new HtmlWebPackPlugin({
-			template: "src/client/index.html"
+			template: 'src/client/index.html'
 		})
 	]
 };
 
 const server = {
 	entry: {
-		"server": "./src/server/index.js"
+		'server': './src/server/index.js'
 	},
-	target: "node",
+	target: 'node',
 	output: {
-		filename: "[name].js",
-		path: path.resolve(__dirname, "dist")
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'dist')
 	},
 	module: moduleObj,
 	externals: [nodeExternals()]
